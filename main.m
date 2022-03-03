182534d080bbb6fe419f730bfa730e9c540578b5
zyan <zhengxi1yan@gmail.com>
2022-03-03 17:15:28 -0500

    add comments to parameters

--- main.m
+++ main.m
@@ -1,16 +1,18 @@
 
-
+% range of integration
 xmin = -3.5;
 xmax = 3.5;
 % double(int((xi(x,3)).^2,x,[xmin,xmax]))
+% small number to avoid zero in denominator
 eps=.1;
 % Strength of repulsion (charge)
 q=1;
+%integration steps
 steps = 25;
-
+%initial energy
 E=10;
+%difference in percent with previous energy for convergence determination 
 deltaEpct=1;
-
 % Number of basis functions
 K = 5;
 cpar=ones(K,1)/sqrt(K);
